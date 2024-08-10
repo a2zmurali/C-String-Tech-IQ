06304151886a99f2aee84901aa85e0566efd3d92
Murali <a2zmurali@gmail.com>
2024-08-10 09:04:48 +0530

    Array

---
+++ Reverse an array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_an_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reverse an array using the in-build method | using the Reverse() method
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            Array.Reverse(numbers);
+            Console.WriteLine("Reversed Array : ");
+            foreach (int value in numbers)
+            {
+                Console.WriteLine(value);
+            }
+
+            //Reverse an array without using the Reverse() method
+            int[] arr = { 1, 2, 3, 4, 5 }; 
+            int length = arr.Length - 1;
+            List<int> numbersList = new List<int>();
+
+            while (length >=0)
+            {
+                numbersList.Add(arr[length]);
+                length--;
+            }
+            Console.WriteLine("Reversed Array : " );
+            foreach (int value in numbersList)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+}
---
+++ Sort an array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_an_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 4, 2, 9, 5, 6, 3, 11 };
+            int temp = 0;
+            for (int i = 0; i <= arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j]) //  if (arr[i] < arr[j]) desc
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Array sort in asscending order");
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
