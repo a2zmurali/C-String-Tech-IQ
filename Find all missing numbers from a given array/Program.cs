0b27f7c661d8ea70dac4231b95d49864ec68b538
Murali <a2zmurali@gmail.com>
2024-08-11 21:56:57 +0530

    Sum_of_array_Find_target

--- Find all missing numbers from a given array/Program.cs
+++ Find all missing numbers from a given array/Program.cs
@@ -10,31 +10,26 @@ namespace Find_all_missing_numbers_from_a_given_array
     {
         static void Main(string[] args)
         {
-            int[] array = { 1 ,3,5,5,7 };
-
+            int[] array = { 1, 3, 5, 5, 7 };
             int temp = 0;
 
             //sort the array
-
-            for(int i = 0; i < array.Length ;  i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                for (int j = i + 1  ; j < array.Length; j++)
+                for (int j = i + 1; j < array.Length; j++)
                 {
-                    if(array[i] > array[j])
+                    if (array[i] > array[j])
                     {
                         temp = array[i];
-                        array[i]= array[j];
+                        array[i] = array[j];
                         array[j] = temp;
                     }
                 }
-
             }
-           
+
 
             // remove duplicate from array 
-
             List<int> uniqueNumbers = new List<int>();
-
             foreach (int number in array)
             {
                 // Check if the list already contains the element
@@ -45,14 +40,11 @@ namespace Find_all_missing_numbers_from_a_given_array
                 }
             }
 
-            //
-
-          
+            //          
             // Actual logic gones here
-
             int diff = array[0] - 0;
 
-            for(int i = 0; i< array.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 // Check if diff and arr[i]-i
                 // both are equal or not
@@ -63,15 +55,13 @@ namespace Find_all_missing_numbers_from_a_given_array
                     // missing elements
                     while (diff < array[i] - i)
                     {
-                        Console.WriteLine( i + diff +" ");
+                        Console.WriteLine(i + diff + " ");
                         diff++;
                     }
                 }
-            }           
-    
-        Console.ReadLine();
+            }
 
-
+            Console.ReadLine();
         }
-}
+    }
 }
---
+++ Sum of array Find target/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_array_Find_target
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4 ,5 , 6};
+            int lenght = array.Length;
+            int traget = 7;
+
+            List<int[]> listarray = new List<int[]>();
+
+            for( int i = 0; i < lenght -1; i++)
+            {
+                for (int j = i +1; j < lenght ; j++)
+                {
+                    if(array[i] + array [j] == traget)
+                    {
+                        int[] ary = { array[i], array[j] };
+                        listarray.Add(ary);
+                       // listarray.Add(new int[] { array[i], array[j] });
+                    }
+                }
+            }
+
+            foreach (var a in listarray)
+            {
+                Console.WriteLine(string.Join(", ", a));
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
